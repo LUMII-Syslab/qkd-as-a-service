2fbb3df6c5d468c9a2ee32bf0a99b8e13d35d8f8
KrisjanisP <krisjanispetrucena@gmail.com>
2022-12-01 14:40:25 +0200

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module qkd-server
+
+go 1.19
+
+require github.com/pebbe/zmq4 v1.2.9
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	zmq "github.com/pebbe/zmq4"
+)
+
+const ClavisKeyUrl = "tcp://*:5555" //"tcp://192.168.10.101:5560"
+
+func main() {
+	zctx, err := zmq.NewContext()
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	zs, err := zctx.NewSocket(zmq.SUB)
+	if err != nil {
+		log.Panicln(err)
+	}
+	log.Println("created a new zeromq socket")
+
+	err = zs.Bind(ClavisKeyUrl)
+	if err != nil {
+		log.Panicln(err)
+	}
+	log.Println("binded zeromq socket to", ClavisKeyUrl)
+
+	for {
+		// Wait for next request from client
+		msg, err := zs.Recv(0)
+		if err != nil {
+			log.Panicln(err)
+		}
+
+		log.Printf("Received %s\n", msg)
+
+		// Do some 'work'
+		time.Sleep(time.Second * 1)
+
+		// Send reply back to client
+		zs.Send("World", 0)
+	}
+}
