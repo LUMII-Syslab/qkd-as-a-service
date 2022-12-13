6fef8c65c0ad85b6ba8e7dba9fdb750d34b8e72a
KrisjanisP <krisjanispetrucena@gmail.com>
2022-12-13 14:10:18 +0200

    add one more test

--- src/utils/utils_test.go
+++ src/utils/utils_test.go
@@ -12,4 +12,10 @@ func TestIntToBytes(t *testing.T) {
 	if !reflect.DeepEqual(want, result) {
 		t.Fatalf(`IntToBytes(%v) = %v, wanted %v`, param, result, want)
 	}
+	param = 2344
+	want = []byte{0b1001, 0b00101000}
+	result = IntToBytes(param)
+	if !reflect.DeepEqual(want, result) {
+		t.Fatalf(`IntToBytes(%v) = %v, wanted %v`, param, result, want)
+	}
 }
