9969513336d39b83e017cd352f187bf2e8691dd1
KrisjanisP <krisjanispetrucena@gmail.com>
2023-01-23 12:48:29 +0200

    fix restart watching keys

--- centis/static/watch.js
+++ centis/static/watch.js
@@ -6,23 +6,34 @@ var brencis_watch_ws = null;
 $(() => {
     $("#toggle-monitor").click(toggle_monitor);
 
-    setInterval(async ()=>{
-        if(watching_keys) {
-            try{
-            let key = await fetch_key();
-            add_key_to_table(key);
-            }catch(e) {
-                console.error("error: ",e);
+    let fetching = 0
+    setInterval(async () => {
+        if (watching_keys) {
+            try {
+                if (fetching !== 0) return;
+                fetching++;
+                let key = await fetch_key();
+                add_key_to_table(key);
+                fetching--;
+            } catch (e) {
+                console.error("error: ", e);
             }
+        } else {
+            fetching = 0;
         }
-    },50);
+    }, 50);
 });
 
 async function toggle_monitor() {
     watching_keys = !watching_keys
-    if(watching_keys) {
+    if (watching_keys) {
         aija_watch_ws = await ws_connect($("#kdcc-aija-url").val())
         brencis_watch_ws = await ws_connect($("#kdcc-brencis-url").val())
+    } else {
+        aija_watch_ws.close();
+        aija_watch_ws = null;
+        brencis_watch_ws.close();
+        brencis_watch_ws = null;
     }
 }
 
@@ -34,13 +45,13 @@ async function fetch_key() {
         hash_left: null,
         hash_right: null,
     }
-    let rkagkh_raw = await ws_send_request(aija_watch_ws, encode_rkagkh_request(256,0))
+    let rkagkh_raw = await ws_send_request(aija_watch_ws, encode_rkagkh_request(256, 0))
     let rkagkh_resp = parse_rkagkh_result(rkagkh_raw)
     result.key_id = rkagkh_resp.key_id
     result.key_left = rkagkh_resp.key_half
     result.hash_right = rkagkh_resp.other_hash
     let key_id_str = hex_octets(rkagkh_resp.key_id)
-    let gkh_raw = await ws_send_request(brencis_watch_ws, encode_gkh_request(256,key_id_str,0))
+    let gkh_raw = await ws_send_request(brencis_watch_ws, encode_gkh_request(256, key_id_str, 0))
     let gkh_resp = parse_gkh_result(gkh_raw)
     result.key_right = gkh_resp.key_half
     result.hash_left = gkh_resp.other_hash
