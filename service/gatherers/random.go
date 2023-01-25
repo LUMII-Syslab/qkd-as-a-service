88dae92702f7416ce9e16b3a24f1eb5b42365607
KrisjanisP <krisjanispetrucena@gmail.com>
2023-01-25 21:48:14 +0200

    lower the load on the system

--- service/gatherers/random.go
+++ service/gatherers/random.go
@@ -17,7 +17,7 @@ func NewRandomKeyGatherer(keyIdLength, keyValLength int) *RandomKeyGatherer {
 }
 
 func (kg *RandomKeyGatherer) Start() error {
-	genTicker := time.NewTicker(time.Microsecond)
+	genTicker := time.NewTicker(time.Millisecond)
 	logTicker := time.NewTicker(5 * time.Second)
 	for {
 		select {
