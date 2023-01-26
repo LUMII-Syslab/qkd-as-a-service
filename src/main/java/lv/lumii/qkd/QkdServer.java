c840572c4207b2d9e234cbf8c19eb48f7c049d68
Sergejs Kozlovičs <sergejs.kozlovics@lumii.lv>
2023-01-27 01:25:03 +0200

    started implementing QkdServer

--- src/main/java/lv/lumii/qkd/QkdServer.java
+++ src/main/java/lv/lumii/qkd/QkdServer.java
@@ -1,20 +1,60 @@
 package lv.lumii.qkd;
 
 import lv.lumii.qrng.ClientBuffer;
+import lv.lumii.qrng.QrngClient;
 import lv.lumii.qrng.QrngProperties;
 import nl.altindag.ssl.SSLFactory;
+import org.bouncycastle.pqc.InjectablePQC;
 import org.cactoos.Scalar;
+import org.cactoos.scalar.Sticky;
+import org.cactoos.scalar.Synced;
 import org.java_websocket.WebSocket;
 import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.net.ssl.SSLContext;
+import java.io.File;
 import java.net.InetSocketAddress;
+import java.security.NoSuchAlgorithmException;
+import java.security.Provider;
 import java.security.SecureRandom;
 
 public class QkdServer {
 
-    public static Logger logger = QkdServer.logger; // one common logger
+    public static Logger logger; // one common logger
+
+    public static String mainExecutable;
+    public static String mainDirectory;
+
+    static {
+
+        InjectablePQC.inject(); // makes BouncyCastlePQCProvider the first and BouncyCastleJsseProvider the second
+
+        File f = new File(QkdServer.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+        mainExecutable = f.getAbsolutePath();
+        mainDirectory = f.getParent();
+
+        // Fix for debug purposes when qrng-client is launched from the IDE:
+        if (mainExecutable.replace('\\', '/').endsWith("/build/classes/java/main")) {
+            mainDirectory = mainExecutable.substring(0, mainExecutable.length()-"/build/classes/java/main".length());
+            mainExecutable = "java";
+        }
+
+        String logFileName = mainDirectory+ File.separator+"qkd.log";
+        System.setProperty("org.slf4j.simpleLogger.logFile", logFileName);
+        logger = LoggerFactory.getLogger(QrngClient.class);
+
+        Provider tlsProvider = null;
+        try {
+            tlsProvider = SSLContext.getInstance("TLS").getProvider();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        logger.info("QkdServer is using TLS provider: "+tlsProvider.getName()); // BCJSSE
+
+    }
 
     //private QkdProperties qkdProperties;
     //private ClientBuffer clientBuffer;
@@ -22,8 +62,9 @@ public class QkdServer {
     private int port = 2222;
     private Scalar<WebSocketServer> wsserver;
 
-    public QkdServer(QrngProperties qrngProperties1, ClientBuffer clientBuffer) {
+    public QkdServer() { // QkdProperties qkdProperties1) {
         System.out.println(" New QkdServer");
+        this.wsserver = new Synced<>(new Sticky<>(() -> newConnection() ));
 
     }
 
@@ -38,7 +79,7 @@ public class QkdServer {
         //TrustManagerFactory trustMgrFact = TrustManagerFactory.getInstance("SunX509");
         //trustMgrFact.init(qrngProperties.trustStore());
 
-        SSLFactory sslf2 = SSLFactory.builder()
+        /*SSLFactory sslf2 = SSLFactory.builder()
                 //.withIdentityMaterial(token.key(), token.password(), token.certificateChain())
                 //.withNeedClientAuthentication()
                 //.withWantClientAuthentication()
@@ -47,7 +88,7 @@ public class QkdServer {
                 .withSecureRandom(SecureRandom.getInstanceStrong())
                 .withCiphers("TLS_AES_256_GCM_SHA384")
                 .build();
-
+*/
 
 
         WebSocketServer wssrv = new WebSocketServer(new InetSocketAddress(port)) {
@@ -84,11 +125,22 @@ public class QkdServer {
             public void onClose(WebSocket arg0, int arg1, String arg2, boolean arg3) {
             }
         };
+
+        wssrv.setConnectionLostTimeout(20);
+        //wssrv.set .setSocketFactory(sslf2.getSslSocketFactory());
         //Start Server functionality
         wssrv.start();
         System.out.println("Server started and ready.");
 
         return wssrv;
+    }
+
+    public void start() {
+        try {
+            this.wsserver.value(); // init the value
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
--- src/test/java/lv/lumii/qrng/QrngTestServer.java
+++ src/test/java/lv/lumii/qrng/QrngTestServer.java
@@ -1,5 +1,6 @@
 package lv.lumii.qrng;
 
+import lv.lumii.qkd.QkdServer;
 import org.graalvm.nativeimage.c.type.CCharPointer;
 
 
@@ -7,12 +8,8 @@ public class QrngTestServer {
 
     public static void main(String[] args) throws Exception {
 
-        QrngClient.qrng_connect(null);
-
-        // Here will be exceptions regarding writing to targetBuffer, which is null and cannot be used from Java
-        // (but only from an executable compiled by GraalVM Native Image).
-        // Still, we can use this code to debug QrngClient.
-        CCharPointer res = QrngClient.qrng_get_random_bytes(null, QrngClient.NULL_BUFFER, 10);
+        QkdServer srv = new QkdServer();
+        srv.start();
 
     }
 
