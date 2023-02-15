df386b97fa9d5bc29e9459cc745657b882658067
KrisjanisP <krisjanispetrucena@gmail.com>
2023-02-15 14:26:31 +0200

    add another statistics chart

--- centis/src/components/StatisticsChart.tsx
+++ centis/src/components/StatisticsChart.tsx
@@ -7,36 +7,47 @@ import {decodeGetStateResponse, encodeGetStateRequest, GetStateRequest} from "..
 export default function StatisticsChart() {
     const config = useContext(ConfigContext)
 
-    let chartCanvas = useRef();
+    let reservableChartRef = useRef();
+    let pressureChartRef = useRef();
 
     useEffect(() => {
-        let chart = new Chart(chartCanvas.current as ChartItem, {
+        let reservableChart = new Chart(reservableChartRef.current as ChartItem, {
             type: 'line',
             data: {
                 labels: [],
-                datasets: [{
-                    label: 'Aija reservable Keys',
-                    data: [],
-                    tension: 0.3,
-                },
-                    {
-                        label: 'Brencis reservable Keys',
-                        data: [],
-                        tension: 0.3,
-
-                    }
-                ]
+                datasets: [{label: 'Aija reservable Keys', data: [], tension: 0.3, pointRadius:0},
+                    {label: 'Brencis reservable Keys', data: [], tension: 0.3, pointRadius:0}]
             },
             options: {
-                animation:{
-                    duration: 0
-                },
+                animation: {duration: 0},
                 scales: {
                     x: {
                         ticks: {
                             autoSkip: true,
                             maxTicksLimit: 5,
                         },
+                    }
+                }
+            },
+        });
+        let pressureChart = new Chart(pressureChartRef.current as ChartItem, {
+            type: 'line',
+            data: {
+                labels: [],
+                datasets: [{label: 'keys added', data: [], tension: 0.3},
+                    {label: 'keys served', data: [], tension: 0.3}]
+            },
+            options: {
+                animation: {duration: 0},
+                scales: {
+                    x: {
+                        ticks: {
+                            autoSkip: true,
+                            maxTicksLimit: 5,
+                        }
+                    },
+                    y: {
+                        type: 'logarithmic'
                     }
                 }
             },
@@ -56,20 +67,32 @@ export default function StatisticsChart() {
             console.log(aijaResponse)
             console.log(brencisResponse)
 
-            chart.data.labels.push(new Date().toLocaleString('sv'))
-            chart.data.datasets[0].data.push(aijaResponse.reservable)
-            chart.data.datasets[1].data.push(brencisResponse.reservable)
-            chart.update()
+            reservableChart.data.labels.push(new Date().toLocaleString('sv'))
+            reservableChart.data.datasets[0].data.push(aijaResponse.reservable)
+            reservableChart.data.datasets[1].data.push(brencisResponse.reservable)
+
+            pressureChart.data.labels.push(new Date().toLocaleString('sv'))
+            pressureChart.data.datasets[0].data.push(aijaResponse.keysAdded)
+            pressureChart.data.datasets[1].data.push(brencisResponse.keysServed)
+
+            reservableChart.update()
+            pressureChart.update()
         }, 500)
         return () => {
             clearInterval(interval)
-            chart.destroy();
+            reservableChart.destroy();
+            pressureChart.destroy();
         }
     }, [])
 
     return (
-        <div className="my-4">
-            <canvas ref={chartCanvas}></canvas>
+        <div className="my-4 d-flex flex-wrap h-25">
+            <div className="col-12 col-lg-6 my-1">
+                <canvas ref={reservableChartRef}></canvas>
+            </div>
+            <div className="col-12 col-lg-6 my-1">
+                <canvas ref={pressureChartRef}></canvas>
+            </div>
         </div>
     )
 }
