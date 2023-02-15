a8b48d954969a9cf67740ec454aa5cf98209c8f1
KrisjanisP <krisjanispetrucena@gmail.com>
2023-02-15 11:49:17 +0200

    divide reservable keys into each respective kdc

--- centis/src/components/StatisticsChart.tsx
+++ centis/src/components/StatisticsChart.tsx
@@ -1,7 +1,10 @@
 import Chart, {ChartItem} from 'chart.js/auto';
-import {useEffect, useRef} from "react";
+import {useContext, useEffect, useRef} from "react";
+import {ConfigContext} from "../utils/config-context";
 
 export default function StatisticsChart() {
+    const config = useContext(ConfigContext)
+
     let chartCanvas = useRef();
 
     useEffect(() => {
@@ -10,16 +13,30 @@ export default function StatisticsChart() {
             data: {
                 labels: ['1', '2', '3'],
                 datasets: [{
-                    label: 'reservable keys',
+                    label: 'Aija reservable Keys',
                     data: [12, 19, 3],
-                    tension: 0.1,
-                }]
+                    tension: 0.3,
+                },
+                    {
+                        label: 'Brencis reservable Keys',
+                        data: [2, 9, 13],
+                        tension: 0.3,
+
+                    }
+                ]
             }
         });
+        let interval = setInterval(() => {
+            // let aijaWs = new WebSocket(config.aijaEndpoint);
+            chart.data.labels.push((chart.data.labels.length + 1).toString())
+            chart.data.datasets[0].data.push(Math.floor(Math.random() * 100))
+            chart.update()
+        }, 1000)
         return () => {
+            clearInterval(interval)
             chart.destroy();
         }
-    },[])
+    }, [])
 
     return (
         <div>
