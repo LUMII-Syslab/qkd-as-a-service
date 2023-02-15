e1fd38329ed46d1ef9b550aea47b2698fc7b70cb
KrisjanisP <krisjanispetrucena@gmail.com>
2023-02-15 13:16:43 +0200

    activate statistics chart

--- centis/src/components/StatisticsChart.tsx
+++ centis/src/components/StatisticsChart.tsx
@@ -2,6 +2,7 @@ import Chart, {ChartItem} from 'chart.js/auto';
 import {useContext, useEffect, useRef} from "react";
 import {ConfigContext} from "../utils/config-context";
 import {wsConnect, wsSendRequest} from "../utils/promise-ws";
+import {decodeGetStateResponse, encodeGetStateRequest, GetStateRequest} from "../utils/get-state-req";
 
 export default function StatisticsChart() {
     const config = useContext(ConfigContext)
@@ -15,26 +16,43 @@ export default function StatisticsChart() {
                 labels: ['1', '2', '3'],
                 datasets: [{
                     label: 'Aija reservable Keys',
-                    data: [12, 19, 3],
+                    data: [],
                     tension: 0.3,
                 },
                     {
                         label: 'Brencis reservable Keys',
-                        data: [2, 9, 13],
+                        data: [],
                         tension: 0.3,
 
                     }
                 ]
+            },
+            options: {
+                animation:{
+                    duration: 0
+                }
             }
         });
         let interval = setInterval(async () => {
-            let aijaWs = await wsConnect(config.aijaEndpoint)
-            let brencisWS = await wsConnect(config.brencisEndpoint);
+            let aijaWs = await wsConnect(config.aijaEndpoint) as WebSocket
+            let brencisWS = await wsConnect(config.brencisEndpoint) as WebSocket
+
+            let encodedRequest = encodeGetStateRequest({cNonce: Math.floor(Math.random() * 100000)} as GetStateRequest)
+
+            let aijaResponse = decodeGetStateResponse(await wsSendRequest(aijaWs, encodedRequest))
+            let brencisResponse = decodeGetStateResponse(await wsSendRequest(brencisWS, encodedRequest))
+
+            aijaWs.close()
+            brencisWS.close()
+
+            console.log(aijaResponse)
+            console.log(brencisResponse)
 
             chart.data.labels.push((chart.data.labels.length + 1).toString())
-            chart.data.datasets[0].data.push(Math.floor(Math.random() * 100))
+            chart.data.datasets[0].data.push(aijaResponse.reservable)
+            chart.data.datasets[1].data.push(brencisResponse.reservable)
             chart.update()
-        }, 1000)
+        }, 500)
         return () => {
             clearInterval(interval)
             chart.destroy();
