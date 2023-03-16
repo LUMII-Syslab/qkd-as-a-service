fce3054f21d826111b49292bdb23b201716705f5
Sergejs Kozlovičs <sergejs.kozlovics@gmail.com>
2023-03-17 01:52:11 +0200

    gen script now uses ECC, not RSA (since RSA cannot be used for key exchange in TLSv1.3)

--- scripts/_centis-gen.sh
+++ scripts/_centis-gen.sh
@@ -5,14 +5,6 @@ set -Eeuo pipefail
 export MY_DIR=`dirname $0`
 export CA_DIR=$MY_DIR/../ca-scripts
 
-# initializing localhost proxy CA and signing proxy-aija, proxy-brencis and proxy-client-centis
-#echo "===> Checking/generating the proxy CA key pair..."
-#[ -d $CA_DIR/proxy-ca ] || $CA_DIR/ca_init.sh proxy-ca RSA $MY_DIR/proxy-ca.cnf
-#echo "===> Checking/generating the Centis proxy client key pair..."
-#[ -d $CA_DIR/proxy-client-centis ] || $CA_DIR/new_client_key.sh proxy-ca proxy-client-centis 3650
-#echo "===> Checking/generating key pairs for proxy-aija and proxy-brencis..."
-#[ -f $MY_DIR/proxy-aija.pem ] || $CA_DIR/new_server_key.sh proxy-ca $MY_DIR/server.cnf 3650 $MY_DIR/proxy-aija.pem
-#[ -f $MY_DIR/proxy-brencis.pem ] || $CA_DIR/new_server_key.sh proxy-ca $MY_DIR/server.cnf 3650 $MY_DIR/proxy-brencis.pem
 
 # initializing PQC CA for Centis and signing Centis client PQC certificate
 echo "===> Checking/generating the PQC CA for Centis key pair..."
@@ -30,25 +22,24 @@ echo "===> Checking/generating the Aija and Brencis server PQC key pairs..."
 [ -d $CA_DIR/aija ] || $CA_DIR/new_server_key.sh ca aija $MY_DIR/pqc-aija.cnf
 [ -d $CA_DIR/brencis ] || $CA_DIR/new_server_key.sh ca brencis $MY_DIR/pqc-brencis.cnf
 
-#if [ ! -f $MY_DIR/centis.pem ]; then
-#  cat $CA_DIR/centis/client.crt > $MY_DIR/centis.pem
-#  cat $CA_DIR/centis/client.key >> $MY_DIR/centis.pem
-#fi
+# Creating ECC keys for classical browsers.
+# Important: do not use RSA, since RSA has been abandoned for signing ServerHello in TLSv1.3. Although RSA can be used
+# for authentication, the TLSv1.3 ServerHello message is usually signed by the same server key.
+ 
+# Initializing localhost proxy CA and signing proxy-aija, proxy-brencis and proxy-client-centis
+echo "===> Checking/generating the proxy CA key pair (ECC, not RSA!)..."
 
-#if [ ! -f $MY_DIR/centis.pem ]; then
-#  cat $CA_DIR/centis/client.crt > $MY_DIR/centis.pem
-#  cat $CA_DIR/centis/client.key >> $MY_DIR/centis.pem
-#fi
+export ALG=EC
+export ALG_ARGS="-pkeyopt ec_paramgen_curve:P-256"
+[ -d $CA_DIR/proxy-ca ] || $CA_DIR/ca_init.sh proxy-ca $ALG $MY_DIR/proxy-ca.cnf "$ALG_ARGS" "$ALG_ARGS" "$ALG_ARGS"
+echo "===> Checking/generating the Centis proxy client key pair..."
+[ -d $CA_DIR/proxy-client-centis ] || $CA_DIR/new_client_key.sh proxy-ca proxy-client-centis $MY_DIR/proxy-client.cnf
+echo "===> Checking/generating key pairs for proxy-aija and proxy-brencis..."
+[ -f $MY_DIR/proxy-aija.pem ] || $CA_DIR/new_server_key.sh proxy-ca proxy-aija $MY_DIR/proxy-server.cnf
+[ -f $MY_DIR/proxy-brencis.pem ] || $CA_DIR/new_server_key.sh proxy-ca proxy-brencis $MY_DIR/proxy-server.cnf
 
-#if [ ! -f $MY_DIR/proxy-client-centis.pem ]; then
-#  cat $CA_DIR/proxy-client-centis/client.crt > $MY_DIR/proxy-client-centis.pem
-#  cat $CA_DIR/proxy-client-centis/client.key >> $MY_DIR/proxy-client-centis.pem
-#fi
-#if [ ! -f $MY_DIR/proxy-client-centis.pfx ]; then
-#  cp $CA_DIR/proxy-client-centis/client.pfx $MY_DIR/proxy-client-centis.pfx
-#fi
 
-echo "===> PLEASE, INSTALL ${MY_DIR}/proxy-client-centis.pem (.pfx) AS YOUR CLIENT KEY!"
+[ ! -d $CA_DIR/proxy-client-centis ] || echo "===> PLEASE, INSTALL ${MY_DIR}/proxy-client-centis.pem (.pfx) AS YOUR CLIENT KEY in your non-PQC browser!"
 echo "===> Running HAProxy..."
 echo "     connect to ws://localhost:8000/ws for aija"
 #echo "     connect to ws://localhost:444/ws for brencis"
