235fab99c974880390bc06e5d4989985da14b058
Sergejs Kozlovičs <sergejs.kozlovics@gmail.com>
2023-04-21 13:29:25 +0300

    reorganized folders before mergning qkduserlib

--- ca-scripts/subtree_init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DIR=`dirname $0`
-pushd $DIR/..
-
-git remote add -f ca-scripts-upstream https://github.com/LUMII-Syslab/ca-scripts.git
-git subtree add --prefix ca-scripts ca-scripts-upstream main --squash
-
-popd
--- ca-scripts/subtree_update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DIR=`dirname $0`
-pushd $DIR/..
-
-git fetch ca-scripts-upstream main
-git subtree pull --prefix ca-scripts ca-scripts-upstream main --squash
-
-popd
