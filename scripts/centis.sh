b12350df8da7f87cce993604d4052970a053b9a7
KrisjanisP <krisjanispetrucena@gmail.com>
2023-06-28 14:44:43 +0300

    rename local proxy to forward proxy

--- scripts/centis.sh
+++ scripts/centis.sh
@@ -24,13 +24,13 @@ fi
 export JAVA_LP="/opt/oqs/lib:/usr/local/lib:$PROJ_ROOT/.libs"
 export MAIN_CLASS=lv.lumii.pqproxy.PQProxy
 
-# Launching PQC reverse proxies in front of Aija and Brencis (to be accessed from Users 1 and 2)
+# Launching PQC forward proxies in front of Aija and Brencis (to be accessed from Users 1 and 2)
 
 export ARGS="-f $MYDIR/pqproxy-local2pqccentis4aija.properties"
-$MYDIR/term_with_title.sh "CENTIS-LOCAL-PROXY-TO-AIJA" "java -cp $JAVA_CP -Djava.library.path=$JAVA_LP $MAIN_CLASS $ARGS"
+$MYDIR/term_with_title.sh "CENTIS-FORWARD-PROXY-TO-AIJA" "java -cp $JAVA_CP -Djava.library.path=$JAVA_LP $MAIN_CLASS $ARGS"
 
 export ARGS="-f $MYDIR/pqproxy-local2pqccentis4brencis.properties"
-$MYDIR/term_with_title.sh "CENTIS-LOCAL-PROXY-TO-BRENCIS" "java -cp $JAVA_CP -Djava.library.path=$JAVA_LP $MAIN_CLASS $ARGS"
+$MYDIR/term_with_title.sh "CENTIS-FORWARD-PROXY-TO-BRENCIS" "java -cp $JAVA_CP -Djava.library.path=$JAVA_LP $MAIN_CLASS $ARGS"
 
 # Launching Centis
 $MYDIR/term_with_title.sh "CENTIS BACKEND" "cd $MYDIR/../centis && npm start"
