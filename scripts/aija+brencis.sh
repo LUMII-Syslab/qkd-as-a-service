862f09c5a63a5a5dee3a525b731d3f519c144379
KrisjanisP <krisjanispetrucena@gmail.com>
2023-07-05 16:16:23 +0300

    use tmux in aija_brencis.sh

--- scripts/aija+brencis.sh
+++ scripts/aija+brencis.sh
@@ -24,21 +24,49 @@ fi
 export JAVA_LP="/opt/oqs/lib:/usr/local/lib:$PROJ_ROOT/.libs"
 export MAIN_CLASS=lv.lumii.pqproxy.PQProxy
 
-# Launching PQC reverse proxies in front of Aija and Brencis (to be accessed from Users 1 and 2)
-export ARGS="-f $MYDIR/pqproxy-pqcuser2aija.properties"
-$MYDIR/term_with_title.sh "USERS-REVERSE-PROXY-TO-AIJA" "java -cp $JAVA_CP -Djava.library.path=$JAVA_LP $MAIN_CLASS $ARGS"
 
-export ARGS="-f $MYDIR/pqproxy-pqcuser2brencis.properties"
-$MYDIR/term_with_title.sh "USERS-REVERSE-PROXY-TO-BRENCIS" "java -cp $JAVA_CP -Djava.library.path=$JAVA_LP $MAIN_CLASS $ARGS"
+if command -v tmux >/dev/null 2>&1; then
+    # tmux is available on the system
+    TMUX_SCRIPTS=$MYDIR/tmux
 
-# Launching PQC reverse proxies in front of Aija and Brencis (to be accessed from Centis)
-export ARGS="-f $MYDIR/pqproxy-pqccentis2aija.properties"
-$MYDIR/term_with_title.sh "CENTIS-REVERSE-PROXY-TO-AIJA" "java -cp $JAVA_CP -Djava.library.path=$JAVA_LP $MAIN_CLASS $ARGS"
+    $TMUX_SCRIPTS/tmux_new_session.sh "aija_brencis"
 
-export ARGS="-f $MYDIR/pqproxy-pqccentis2brencis.properties"
-$MYDIR/term_with_title.sh "CENTIS-REVERSE-PROXY-TO-BRENCIS" "java -cp $JAVA_CP -Djava.library.path=$JAVA_LP $MAIN_CLASS $ARGS"
+    # Launching PQC reverse proxies in front of Aija and Brencis (to be accessed from Users 1 and 2)
+    export ARGS="-f $MYDIR/pqproxy-pqcuser2aija.properties"
+    $TMUX_SCRIPTS/tmux_add_window.sh "aija_brencis" "USERS-REVERSE-PROXY-TO-AIJA" "java -cp $JAVA_CP -Djava.library.path=$JAVA_LP $MAIN_CLASS $ARGS"
 
-# Launching both Aija and Brencis
-$MYDIR/term_with_title.sh "GO AIJA+BRENCIS" "cd $MYDIR/..//service && go mod tidy && go run ."
+    export ARGS="-f $MYDIR/pqproxy-pqcuser2brencis.properties"
+    $TMUX_SCRIPTS/tmux_add_window.sh "aija_brencis" "USERS-REVERSE-PROXY-TO-BRENCIS" "java -cp $JAVA_CP -Djava.library.path=$JAVA_LP $MAIN_CLASS $ARGS"
+
+    # Launching PQC reverse proxies in front of Aija and Brencis (to be accessed from Centis)
+    export ARGS="-f $MYDIR/pqproxy-pqccentis2aija.properties"
+    $TMUX_SCRIPTS/tmux_add_window.sh "aija_brencis" "CENTIS-REVERSE-PROXY-TO-AIJA" "java -cp $JAVA_CP -Djava.library.path=$JAVA_LP $MAIN_CLASS $ARGS"
+
+    export ARGS="-f $MYDIR/pqproxy-pqccentis2brencis.properties"
+    $TMUX_SCRIPTS/tmux_add_window.sh "aija_brencis" "CENTIS-REVERSE-PROXY-TO-BRENCIS" "java -cp $JAVA_CP -Djava.library.path=$JAVA_LP $MAIN_CLASS $ARGS"
+
+    # Launching both Aija and Brencis
+    $TMUX_SCRIPTS/tmux_add_window.sh "aija_brencis" "GO_AIJA_BRENCIS" "cd $MYDIR/..//service && go mod tidy && go run ."
+
+    # Attach to the session
+    $MYDIR/term_with_title.sh "aija+brencis.sh" "tmux attach -t aija_brencis"
+else
+    # Launching PQC reverse proxies in front of Aija and Brencis (to be accessed from Users 1 and 2)
+    export ARGS="-f $MYDIR/pqproxy-pqcuser2aija.properties"
+    $MYDIR/term_with_title.sh "USERS-REVERSE-PROXY-TO-AIJA" "java -cp $JAVA_CP -Djava.library.path=$JAVA_LP $MAIN_CLASS $ARGS"
+
+    export ARGS="-f $MYDIR/pqproxy-pqcuser2brencis.properties"
+    $MYDIR/term_with_title.sh "USERS-REVERSE-PROXY-TO-BRENCIS" "java -cp $JAVA_CP -Djava.library.path=$JAVA_LP $MAIN_CLASS $ARGS"
+
+    # Launching PQC reverse proxies in front of Aija and Brencis (to be accessed from Centis)
+    export ARGS="-f $MYDIR/pqproxy-pqccentis2aija.properties"
+    $MYDIR/term_with_title.sh "CENTIS-REVERSE-PROXY-TO-AIJA" "java -cp $JAVA_CP -Djava.library.path=$JAVA_LP $MAIN_CLASS $ARGS"
+
+    export ARGS="-f $MYDIR/pqproxy-pqccentis2brencis.properties"
+    $MYDIR/term_with_title.sh "CENTIS-REVERSE-PROXY-TO-BRENCIS" "java -cp $JAVA_CP -Djava.library.path=$JAVA_LP $MAIN_CLASS $ARGS"
+
+    # Launching both Aija and Brencis
+    $MYDIR/term_with_title.sh "GO AIJA+BRENCIS" "cd $MYDIR/..//service && go mod tidy && go run ."
+fi
 
 popd
